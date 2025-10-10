a8561a994087687d42ea6491b37a3d74251e40f1
arshet2 <arshet2@gmail.com>
2025-10-10 17:44:50 -0500

    scripts Salto(casta tutor)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 private Rigidbody2D rb;
  
     public float velocidadDemovimiento = 10;
+    public float fuerzaDeSalto = 5;
+
+    private Vector2 direccion; 
 
 
     private void Awake() 
@@ -34,12 +37,36 @@ private Rigidbody2D rb;
         float y = Input.GetAxis("Vertical");
 
 
-        Vector2 direccion = new Vector2(x, y);
+        direccion = new Vector2(x, y);
         
-        Caminar(direccion);
+        Caminar();
+
+        MejorarSalto();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            saltar();
+        }    
     }
 
-    private void Caminar(Vector2 direccion)
+    private void MejorarSalto()
+    {
+      if(rb.linearVelocity.y < 0)
+      {
+        rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (2.5f - 1) * Time.deltaTime;
+      }  
+      else if(rb.linearVelocity.y > 0 && !Input.GetKey(KeyCode.Space))
+      {
+        rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (2.0f -1) * Time.deltaTime;                     
+      }
+    }
+
+    private void saltar()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0);
+        rb.linearVelocity +=  Vector2.up * fuerzaDeSalto;
+    }
+
+    private void Caminar()
     {
         rb.linearVelocity = new Vector2(direccion.x * velocidadDemovimiento, rb.linearVelocity.y);
     }
